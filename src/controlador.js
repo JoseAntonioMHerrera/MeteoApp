95cf67085d0df9b6240ccf2e437223ad102649e0
Jose Antonio Muñoz Herrera <joseantoniomunozherrera@gmail.com>
2019-10-09 22:55:55 +0200

    Closes #19

--- src/controlador.js
+++ src/controlador.js
@@ -4,63 +4,119 @@ const mes = require("./mes-schema")
 const ciudad = require("./ciudad-schema");
 
 exports.peticionGETCiudad = function(nombre_ciudad, anio){
+    
     temperaturas_ciudad = {
         "ciudad": "Granada",
         "anio": "2019",
-        "meses":[{
-            "8":[{
-                "dias": [{
-                    "1":[{
-                        "08":"14",
-                        "14":"22",
-                        "21":"17"
-                    }]
-                }]
-            }]
-        }]
+        "meses":[
+            {
+                "8":{
+                    "dias": [
+                        {
+                            "num_dia": "1",
+                            "temperaturas": {
+                                "hora_08":"14",
+                                "hora_14":"22",
+                                "hora_21":"17"
+                            }
+                        },
+                        {
+                            "num_dia": "2",
+                            "temperaturas": {
+                                "hora_08":"14",
+                                "hora_14":"22",
+                                "hora_21":"17"
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                "9":{
+                    "dias": [
+                        {
+                            "num_dia": "23",
+                            "temperaturas": {
+                                "hora_08":"14",
+                                "hora_14":"22",
+                                "hora_21":"17"
+                            }
+                        },
+                        {
+                            "num_dia": "24",
+                            "temperaturas": {
+                                "hora_08":"14",
+                                "hora_14":"22",
+                                "hora_21":"17"
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
     };
+    
 
-    return temperaturas_ciudad;
+
+    return JSON.stringify(temperaturas_ciudad);
 };
 
 exports.peticionGETMes = function(ciudad, anio, mes){
+
     temperaturas_ciudad_mes = {
         "ciudad": "Granada",
         "anio": "2019",
         "mes": "5",
-        "dias": [{
-            "8":[{
-                "08":"14",
-                "14":"22",
-                "21":"17"
-            }],
-            "9":[{
-                "08":"16",
-                "14":"26",
-                "21":"20"
-            }],
-            "1":[{
-                "08":"12",
-                "14":"23",
-                "21":"16"
-            }]
-        }]
+        "dias": [
+            {   
+                "num_dia":"2",
+                "temperaturas":
+                {
+                    "hora_08":"14",
+                    "hora_14":"22",
+                    "hora_21":"17"
+                }
+            },
+            {
+                "num_dia":"3",
+                "temperaturas":
+                {
+                    "hora_08":"14",
+                    "hora_14":"22",
+                    "hora_21":"17"
+                }
+            },
+            {   
+                "num_dia":"4",
+                "temperaturas":
+                {
+                    "hora_08":"14",
+                    "hora_14":"22",
+                    "hora_21":"17"
+                }
+            }
+        ]
     };
+
     return JSON.stringify(temperaturas_ciudad_mes);
 };
 
 exports.peticionGETDia = function(ciudad, anio, mes, dia){
+    
     temperaturas_ciudad_dia = {
         "ciudad": "Granada",
         "anio": "2019",
         "mes": "5",
-        "dias": [{
-            "8":[{
-                "08":"14",
-                "14":"22",
-                "21":"17"
-            }]
-        }]
+        "dias":
+        {
+            "num_dia":"2",
+            "temperaturas":
+            {
+                "hora_08":"14",
+                "hora_14":"22",
+                "hora_21":"17"
+            }
+        }
     };
 
     return JSON.stringify(temperaturas_ciudad_dia);
@@ -68,16 +124,16 @@ exports.peticionGETDia = function(ciudad, anio, mes, dia){
 };
 
 exports.peticionPOSTCiudad = function(payload){
-    let response = Joi.validate(payload,ciudad.ciudad_schema);
-    return (response ? false : true);
+    let response = ciudad.ciudad_schema.validate(payload);
+    return response;
 };
 
 exports.peticionPOSTMes = function(payload){
-    let response = Joi.validate(payload,mes.mes_schema);
-    return (response ? false : true);
+    let response = mes.mes_schema.validate(payload);
+    return response;
 };
 
 exports.peticionPOSTDia = function(payload){
-    let response = Joi.validate(payload,dia.dia_schema);
-    return (response ? false : true);        
+    let response = dia.dia_peticionschema.validate(payload);
+    return response;        
 }
