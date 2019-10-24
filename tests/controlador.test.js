ec782dd37eba47eef5a2af8a555a79976f7416ca
Jose Antonio Muñoz Herrera <joseantoniomunozherrera@gmail.com>
2019-10-24 20:54:40 +0200

    Se han cambiado los métodos que se testeaban a partir del cambio de nombre dichos métodos en controlador.js. Además se ha cambiado las lineas referentes a la respuesta de los métodos esperada en los tests para adecuarlos a la versión de Joi 15.1.1. Closes #44

--- tests/controlador.test.js
+++ tests/controlador.test.js
@@ -1,32 +1,33 @@
 const controlador = require("../src/controlador.js");
 const datos_prueba = require("./datos.js");
 
-test('la respuesta de peticionPOSTCiudad debería tener error como undefined', ()=> {
-    respuesta = controlador.peticionPOSTCiudad(datos_prueba.post_temperaturas_ciudad);
-    expect(respuesta.error).toBe(undefined);
+test('la respuesta de insertarCiudad debería tener error como undefined', ()=> {
+    respuesta = controlador.insertarCiudad(datos_prueba.post_temperaturas_ciudad);
+    console.log(respuesta);
+    expect(respuesta.error).toBe(null);
 });
 
-test('la respuesta de peticionPOSTMes debería tener error como undefined', ()=> {
-    respuesta = controlador.peticionPOSTMes(datos_prueba.temperaturas_ciudad_mes);
-    expect(respuesta.error).toBe(undefined);
+test('la respuesta de insertarMes debería tener error como undefined', ()=> {
+    respuesta = controlador.insertarMes(datos_prueba.temperaturas_ciudad_mes);
+    expect(respuesta.error).toBe(null);
 });
 
-test('la respuesta de peticionPOSTDia debería tener error como undefined', ()=> {
-    respuesta = controlador.peticionPOSTDia(datos_prueba.temperaturas_ciudad_dia);
-    expect(respuesta.error).toBe(undefined);
+test('la respuesta de insertarDia debería tener error como undefined', ()=> {
+    respuesta = controlador.insertarDia(datos_prueba.temperaturas_ciudad_dia);
+    expect(respuesta.error).toBe(null);
 });
 
-test('la respuesta de peticionGETCiudad debería devolver el objeto JSON {datos_prueba.temperaturas_ciudad}', ()=> {
-    respuesta = controlador.peticionGETCiudad("granada","1995");
+test('la respuesta de peticionCiudad debería devolver el objeto JSON {datos_prueba.temperaturas_ciudad}', ()=> {
+    respuesta = controlador.peticionCiudad("granada","1995");
     expect(respuesta).toBe(JSON.stringify(datos_prueba.temperaturas_ciudad));
 });
 
-test('la respuesta de peticionGETMes debería devolver el objeto JSON {datos_prueba.temperaturas_ciudad_mes}', ()=> {
-    respuesta = controlador.peticionGETMes("granada","1995","5");
+test('la respuesta de peticionMes debería devolver el objeto JSON {datos_prueba.temperaturas_ciudad_mes}', ()=> {
+    respuesta = controlador.peticionMes("granada","1995","5");
     expect(respuesta).toBe(JSON.stringify(datos_prueba.temperaturas_ciudad_mes));
 });
 
-test('la respuesta de peticionGETDia debería devolver el objeto JSON {datos_prueba.temperaturas_ciudad_dia}', ()=> {
-    respuesta = controlador.peticionGETDia("granada","1995","5","1");
+test('la respuesta de peticionDia debería devolver el objeto JSON {datos_prueba.temperaturas_ciudad_dia}', ()=> {
+    respuesta = controlador.peticionDia("granada","1995","5","1");
     expect(respuesta).toBe(JSON.stringify(datos_prueba.temperaturas_ciudad_dia));
 });
