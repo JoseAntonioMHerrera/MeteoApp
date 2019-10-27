734eaba579a46c3de0292612ca76ce6ba5b58843
Jose Antonio Muñoz Herrera <joseantoniomunozherrera@gmail.com>
2019-10-27 21:08:43 +0100

    Se comprueba la variable de entorno NODE_ENV para usar la base de datos y url correcta dependiendo de donde y para que proposito se lance la aplicación. Closes #65

--- src/db.js
+++ src/db.js
@@ -1,13 +1,18 @@
 var mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/meteoDB',{ useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify:false });
+let url_conexion = "mongodb://localhost/meteoDB";
+
+if(process.env.NODE_ENV == "docker")
+    url_conexion='mongodb://database/meteoDB';
+else if(process.env.NODE_ENV == "test")
+    url_conexion='mongodb://localhost/meteoDBtest';
+
+mongoose.connect(url_conexion,{ useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify:false });
 
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console,'connection error:'));
-db.once('open',function(){
-
-});
+db.once('open',function(){});
 
 module.exports={
     mongoose_db: db
