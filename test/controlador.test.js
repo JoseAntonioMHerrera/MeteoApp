61bbc0aed687fc1398550d3adf25efc220950789
Jose Antonio Muñoz Herrera <joseantoniomunozherrera@gmail.com>
2019-10-27 03:30:24 +0100

    Se han añadido los tests de los métodos referentes a las operaciones CRUD de controlador.js con Mocha y la biblioteca assert de Chai. Closes #55

---
+++ test/controlador.test.js
@@ -0,0 +1,228 @@
+const assert = require('chai').assert;
+const controlador = require("../src/controlador.js");
+const datos_prueba = require("./datos.js");
+
+
+describe('Test de inserción de ciudad.', ()=>{
+    it('La inserción de una ciudad debe retornar {}', function(done){
+        let promesa_respuesta  = controlador.insertarCiudad(datos_prueba.temperaturas_granada);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.isEmpty(respuesta,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });   
+});
+
+describe('Test de inserción de año.', ()=> {
+    it('La inserción de un año debe retornar {}', function(done){
+        let promesa_respuesta = controlador.insertarAnio(datos_prueba.temperaturas_granada_2018);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.isEmpty(respuesta,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });
+});
+
+describe('Test de inserción de mes', ()=>{
+    it('La inserción de un mes debe retornar {}', function(done){
+        let promesa_respuesta = controlador.insertarMes(datos_prueba.temperaturas_granada_2018_mes_11);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.isEmpty(respuesta,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });
+});
+
+describe('Test de inserción de día', ()=>{
+    it('La inserción de un día debe retornar {}', function(done){
+        let promesa_respuesta = controlador.insertarDia(datos_prueba.temperaturas_granada_2018_mes_11_dia_20);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.isEmpty(respuesta,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });
+});
+
+describe('Test de lectura de ciudad.', ()=>{
+    it('La lectura de una ciudad no debe ser null', function(done){
+        let promesa_respuesta = controlador.peticionCiudad(datos_prueba.granada);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.notEqual(null,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });   
+});
+
+describe('Test de lectura de año.', ()=> {
+    it('La lectura de un año no debe ser null', function(done){
+        let promesa_respuesta = controlador.peticionAnio(datos_prueba.granada, datos_prueba.dosmil18);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.notEqual(null,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });
+});
+
+describe('Test de lectura de mes', ()=>{
+    it('La lectura de un mes no debe ser null', function(done){
+        let promesa_respuesta = controlador.peticionMes(datos_prueba.granada, datos_prueba.dosmil19, datos_prueba.mes12);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.notEqual(null,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });
+});
+
+describe('Test de lectura de día', ()=>{
+    it('La lectura de un día no debe ser null', function(done){
+        let promesa_respuesta = controlador.peticionDia(datos_prueba.granada, datos_prueba.dosmil18, datos_prueba.mes11, datos_prueba.dia20);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.notEqual(null,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });
+});
+
+describe('Test de edición de ciudad.', ()=>{
+    it('La edición de una ciudad debe retornar {}', function(done){
+        let promesa_respuesta  = controlador.actualizarCiudad(datos_prueba.temperaturas_granada_2);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.isEmpty(respuesta,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });   
+});
+
+describe('Test de edición de año.', ()=> {
+    it('La edición de un año debe retornar {}', function(done){
+        let promesa_respuesta = controlador.actualizarAnio(datos_prueba.temperaturas_granada_2018_2);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.isEmpty(respuesta,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });
+});
+
+describe('Test de edición de mes', ()=>{
+    it('La edición de un mes debe retornar {}', function(done){
+        let promesa_respuesta = controlador.actualizarMes(datos_prueba.temperaturas_granada_2018_mes_11_2);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.isEmpty(respuesta,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });
+});
+
+describe('Test de edición de día', ()=>{
+    it('La edición de un día debe retornar {}', function(done){
+        let promesa_respuesta = controlador.actualizarDia(datos_prueba.temperaturas_granada_2018_mes_11_dia_20_2);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.isEmpty(respuesta,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });
+});
+
+describe('Test de eliminación de día', ()=>{
+    it('La eliminación de un día debe retornar {}', function(done){
+        let promesa_respuesta = controlador.eliminarDia(datos_prueba.granada,datos_prueba.dosmil18,datos_prueba.mes11,datos_prueba.dia20);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.isEmpty(respuesta,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });
+});
+
+describe('Test de eliminación de mes', ()=>{
+    it('La eliminación de un mes debe retornar {}', function(done){
+        let promesa_respuesta = controlador.eliminarMes(datos_prueba.granada,datos_prueba.dosmil18,datos_prueba.mes11);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.isEmpty(respuesta,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });
+});
+
+describe('Test de eliminación de año.', ()=> {
+    it('La eliminación de un año debe retornar {}', function(done){
+        let promesa_respuesta = controlador.eliminarAnio(datos_prueba.granada,datos_prueba.dosmil18);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.isEmpty(respuesta,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });
+});
+
+describe('Test de eliminación de ciudad.', ()=>{
+    it('La eliminación de una ciudad debe retornar {}', function(done){
+        let promesa_respuesta  = controlador.eliminarCiudad(datos_prueba.granada);
+        promesa_respuesta.then(function(respuesta){
+            try{
+                assert.isEmpty(respuesta,"Error: " + respuesta.value);
+                done();
+            }catch(error){
+                done(error);
+            }
+        });
+    });   
+});
---
+++ test/datos.js
@@ -0,0 +1,148 @@
+module.exports = {
+    temperaturas_granada : {
+        "ciudad": "Granada",
+        "anios":[
+            {
+                "num_anio": "2019",
+                "meses": [
+                    {
+                        "num_mes":12,
+                        "dias":[
+                            {
+                                "num_dia":23,
+                                "temperaturas":{
+                                    "hora_08":6,
+                                    "hora_14":6,
+                                    "hora_21":6
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    temperaturas_granada_2: {
+        "ciudad": "Granada",
+        "anios":[
+            {
+                "num_anio": "2019",
+                "meses": [
+                    {
+                        "num_mes":12,
+                        "dias":[
+                            {
+                                "num_dia":23,
+                                "temperaturas":{
+                                    "hora_08":6,
+                                    "hora_14":6,
+                                    "hora_21":6
+                                }
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "num_anio": "2020",
+                "meses": [
+                    {
+                        "num_mes":7,
+                        "dias":[
+                            {
+                                "num_dia":20,
+                                "temperaturas":{
+                                    "hora_08":6,
+                                    "hora_14":6,
+                                    "hora_21":6
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    temperaturas_granada_2018_2: {
+        "ciudad":"Granada",
+        "num_anio": 2018,
+        "meses":[
+            {
+                "num_mes": 4,
+                "dias":[]
+            }
+        ]
+    },
+    temperaturas_granada_2018: {
+        "ciudad":"Granada",
+        "num_anio": 2018,
+        "meses":[
+            {
+                "num_mes": 3,
+                "dias":[]
+            }
+        ]
+    },
+    temperaturas_granada_2018_mes_11_2: {
+        "ciudad":"Granada",
+        "num_anio": 2018,
+        "num_mes": 11,
+        "dias":[
+            {
+                "num_dia": 11,
+                "temperaturas":{
+                    "hora_08": 6,
+                    "hora_14": 6,
+                    "hora_21": 6
+                }
+            }
+        ]
+    },
+    temperaturas_granada_2018_mes_11: {
+        "ciudad":"Granada",
+        "num_anio": 2018,
+        "num_mes": 11,
+        "dias":[
+            {
+                "num_dia": 10,
+                "temperaturas":{
+                    "hora_08": 6,
+                    "hora_14": 6,
+                    "hora_21": 6
+                }
+            }
+        ]
+    },
+    temperaturas_granada_2018_mes_11_dia_20_2: {
+        "ciudad":"Granada",
+        "num_anio": 2018,
+        "num_mes": 11,
+        "num_dia": 20,
+        "temperaturas":{
+            "hora_08": 3,
+            "hora_14": 3,
+            "hora_21": 3
+        }
+    },
+    temperaturas_granada_2018_mes_11_dia_20: {
+        "ciudad":"Granada",
+        "num_anio": 2018,
+        "num_mes": 11,
+        "num_dia": 20,
+        "temperaturas":{
+            "hora_08": 6,
+            "hora_14": 6,
+            "hora_21": 6
+        }
+    },
+    malaga: "Malaga",
+    granada: "Granada",
+    dosmil18: 2018,
+    dosmil19: 2019,
+    mes11: 11,
+    mes12: 12,
+    mes7: 7,
+    dia22: 22,
+    dia23: 23,
+    dia20: 20
+}
