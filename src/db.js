e95cb510e065df71cee4ff19d5c233e163efa08c
Jose Antonio Muñoz Herrera <joseantoniomunozherrera@gmail.com>
2019-11-10 00:01:05 +0100

    Se añade la comprobación de si el entorno es Heroku, y asignamos la url que se ubica en otra variable de entorno. Closes #86

--- src/db.js
+++ src/db.js
@@ -6,7 +6,8 @@ if(process.env.NODE_ENV == "docker")
     url_conexion='mongodb://database/meteoDB';
 else if(process.env.NODE_ENV == "test")
     url_conexion='mongodb://localhost/meteoDBtest';
-
+else if(process.env.NODE_ENV == "heroku")
+    url_conexion= process.env.DB_CONN_HEROKU;
 mongoose.connect(url_conexion,{ useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify:false });
 
 var db = mongoose.connection;
