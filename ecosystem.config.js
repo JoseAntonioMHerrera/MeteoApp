b2062a5732fcfc1b4922d2c2c0dde8373f71560d
Jose Antonio Muñoz Herrera <joseantoniomunozherrera@gmail.com>
2019-11-12 01:34:52 +0100

    Añadidas variables de entorno para desarrollo y producción. Closes #107

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,5 +2,11 @@ exports.module = {
     apps:[{
         name: "meteoapp",
         script: "./app.js",
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            NODE_ENV: 'production'
+        }
     }]
 }
