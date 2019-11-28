70a67f727f0a55b0c656c137c5203079641b22a6
Jose Antonio Muñoz Herrera <joseantoniomunozherrera@gmail.com>
2019-11-28 17:15:40 +0100

    Testeando CI de Azure v2

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ server.route({
     path: "/status",
     handler: (request,h) =>{
 //        return h.response({"status":"OK", "ejemplo":{"ruta":"/meteo/Granada","resultado":{"ciudad":"Granada","anios":[{"meses":[{"dias":[{"temperaturas":{"hora_08":6,"hora_14":6,"hora_21":6},"_id":"5dcb2aee0d7ced00175ff196","num_dia":23}],"_id":"5dcb2aee0d7ced00175ff195","num_mes":12}],"_id":"5dcb2aee0d7ced00175ff194","num_anio":2019}],"__v":0}}});
-	  return "Testing"
+	  return "Testing, prueba numero 2"
     }
 });
 
